e657a58a3324c8d48f590491cd3e102e72c3d00a
Aaron Lo <22aaronl@students.harker.org>
2023-04-13 22:05:01 -0500

    updated memory & regs

--- mem.v
+++ mem.v
@@ -1,9 +1,13 @@
 `timescale 1ps/1ps
 
 module mem(input clk,
+    input [15:1]rinstruct0_, output [15:0]routput0_,
+    input [15:1]rinstruct1_, output [15:0]routput1_,
+    input [15:1]rinstruct2_, output [15:0]routput2_,
+    input [15:1]rinstruct3_, output [15:0]routput3_, 
     input [15:1]raddr0_, output [15:0]rdata0_,
     input [15:1]raddr1_, output [15:0]rdata1_,
-    input wen, input [15:1]waddr, input [15:0]wdata);
+    input wen0, input [15:1]waddr0, input [15:0]wdata0);
 
     reg [15:0]data[0:16'h7fff];
 
@@ -11,6 +15,16 @@ module mem(input clk,
     initial begin
         $readmemh("mem.hex",data);
     end
+
+    reg [15:1]rinstruct0;
+    reg [15:1]rinstruct1;
+    reg [15:1]rinstruct2;
+    reg [15:1]rinstruct3;
+
+    assign routput0_ = data[rinstruct0];
+    assign routput1_ = data[rinstruct1];
+    assign routput2_ = data[rinstruct2];
+    assign routput3_ = data[rinstruct3];
 
     reg [15:1]raddr0;
     reg [15:0]rdata0;
@@ -22,12 +36,17 @@ module mem(input clk,
     assign rdata1_ = rdata1;
 
     always @(posedge clk) begin
+        rinstruct0 <= rinstruct0_;
+        rinstruct1 <= rinstruct1_;
+        rinstruct2 <= rinstruct2_;
+        rinstruct3 <= rinstruct3_;
+
         raddr0 <= raddr0_;
         raddr1 <= raddr1_;
         rdata0 <= data[raddr0];
         rdata1 <= data[raddr1];
-        if (wen) begin
-            data[waddr] <= wdata;
+        if (wen0) begin
+            data[waddr0] <= wdata0;
         end
     end
 
--- regs.v
+++ regs.v
@@ -3,21 +3,44 @@
 module regs(input clk,
     input [3:0]raddr0_, output [15:0]rdata0,
     input [3:0]raddr1_, output [15:0]rdata1,
-    input wen, input [3:0]waddr, input [15:0]wdata);
+    input [3:0]raddr2_, output [15:0]rdata2,
+    input [3:0]raddr3_, output [15:0]rdata3,
+    input [3:0]raddr4_, output [15:0]rdata4,
+    input [3:0]raddr5_, output [15:0]rdata5,
+    input wen0, input [3:0]waddr0, input [15:0]wdata0,
+    input wen1, input [3:0]waddr1, input [15:0]wdata1,
+    input wen2, input [3:0]waddr2, input [15:0]wdata2);
 
     reg [15:0]data[0:15];
 
     reg [3:0]raddr0;
     reg [3:0]raddr1;
+    reg [3:0]raddr2;
+    reg [3:0]raddr3;
+    reg [3:0]raddr4;
+    reg [3:0]raddr5;
 
     assign rdata0 = data[raddr0];
     assign rdata1 = data[raddr1];
+    assign rdata2 = data[raddr2];
+    assign rdata3 = data[raddr3];
+    assign rdata4 = data[raddr4];
+    assign rdata5 = data[raddr5];
 
     always @(posedge clk) begin
         raddr0 <= raddr0_;
         raddr1 <= raddr1_;
-        if (wen) begin
-            data[waddr] <= wdata;
+        raddr2 <= raddr2_;
+        raddr3 <= raddr3_;
+        raddr4 <= raddr4_;
+        raddr5 <= raddr5_;
+
+        if (wen0) begin
+            data[waddr0] <= wdata0;
+        end
+
+        if(wen1) begin
+            data[waddr1] <= wdata1;
         end
     end
 
